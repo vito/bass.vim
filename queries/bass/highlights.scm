42afd4ac66957a9a650657b0db2816ffaf419a71
Alex Suraci <suraci.alex@gmail.com>
2023-04-01 18:33:39 -0400

    update queries

--- queries/bass/highlights.scm
+++ queries/bass/highlights.scm
@@ -1,86 +1,77 @@
-; GENERATED VIA https://github.com/vito/tree-sitter-bass
+;; Variables
 
-;;; comments
+(list (symbol) @variable)
 
-(comment) @comment.line
-
-;;; punctuation
-
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket
-
-;;; constants
-
-[(ignore) (null)] @constant.builtin
-
-(bool) @constant.builtin.boolean
-
-(int) @number
-
-;;; strings
-
-;; string literals
-
-(string) @string
-(string (string_escape) @string.escape)
-
-;; keywords (symbol literals)
-
-(keyword) @symbol
-
-;; paths
-
-(dot) @string.special.path
-(dotdot) @string.special.path
-(command) @string.special.path
-(subpath (symbol) @string.special.path)
-
-; slashes in a path denote a combiner call
-(subpath (slash) @function)
-
-
-
-;;; generic highlighting for all forms
-
-; first symbol in a list form is a combiner call
-(list . (symbol) @function)
-
-; highlight symbols as vars only when they're clearly vars
 (cons (symbol) @variable)
+
 (scope (symbol) @variable)
-(path form: (symbol) @variable)
-(symbind form: (symbol) @variable)
 
-;;; specific highlighting for builtins & special forms
+(symbind (symbol) @variable)
 
-;; symbol classification based highlighting
+;; Functions
 
-(list . (symbol) @conditional (#any-of? @conditional "if" "case" "cond" "when"))
-(cons . (symbol) @conditional (#any-of? @conditional "if" "case" "cond" "when"))
+(list
+  . (symbol) @function)
 
-(list . (symbol) @repeat (#any-of? @repeat "each"))
-(cons . (symbol) @repeat (#any-of? @repeat "each"))
+; Keywords construct a symbol
 
-(list . (symbol) @define (#any-of? @define "def" "defop" "defn"))
-(cons . (symbol) @define (#any-of? @define "def" "defop" "defn"))
+(keyword) @constructor
 
-(list . (symbol) @function.builtin (#any-of? @function.builtin "dump" "mkfs" "json" "log" "error" "now" "cons" "wrap" "unwrap" "eval" "make-scope" "bind" "meta" "with-meta" "null?" "ignore?" "boolean?" "number?" "string?" "symbol?" "scope?" "sink?" "source?" "list?" "pair?" "applicative?" "operative?" "combiner?" "path?" "empty?" "thunk?" "+" "*" "quot" "-" "max" "min" "=" ">" ">=" "<" "<=" "list->source" "across" "emit" "next" "reduce-kv" "assoc" "symbol->string" "string->symbol" "str" "substring" "trim" "scope->list" "string->fs-path" "string->cmd-path" "string->dir" "subpath" "path-name" "path-stem" "with-image" "with-dir" "with-args" "with-cmd" "with-stdin" "with-env" "with-insecure" "with-label" "with-port" "with-tls" "with-mount" "thunk-cmd" "thunk-args" "resolve" "start" "addr" "wait" "read" "cache-dir" "binds?" "recall-memo" "store-memo" "mask" "list" "list*" "first" "rest" "length" "second" "third" "map" "map-pairs" "foldr" "foldl" "append" "filter" "conj" "list->scope" "merge" "apply" "id" "always" "vals" "keys" "memo" "succeeds?" "run" "last" "take" "take-all" "insecure!" "from" "cd" "wrap-cmd" "mkfile" "path-base" "not"))
-(cons . (symbol) @function.builtin (#any-of? @function.builtin "dump" "mkfs" "json" "log" "error" "now" "cons" "wrap" "unwrap" "eval" "make-scope" "bind" "meta" "with-meta" "null?" "ignore?" "boolean?" "number?" "string?" "symbol?" "scope?" "sink?" "source?" "list?" "pair?" "applicative?" "operative?" "combiner?" "path?" "empty?" "thunk?" "+" "*" "quot" "-" "max" "min" "=" ">" ">=" "<" "<=" "list->source" "across" "emit" "next" "reduce-kv" "assoc" "symbol->string" "string->symbol" "str" "substring" "trim" "scope->list" "string->fs-path" "string->cmd-path" "string->dir" "subpath" "path-name" "path-stem" "with-image" "with-dir" "with-args" "with-cmd" "with-stdin" "with-env" "with-insecure" "with-label" "with-port" "with-tls" "with-mount" "thunk-cmd" "thunk-args" "resolve" "start" "addr" "wait" "read" "cache-dir" "binds?" "recall-memo" "store-memo" "mask" "list" "list*" "first" "rest" "length" "second" "third" "map" "map-pairs" "foldr" "foldl" "append" "filter" "conj" "list->scope" "merge" "apply" "id" "always" "vals" "keys" "memo" "succeeds?" "run" "last" "take" "take-all" "insecure!" "from" "cd" "wrap-cmd" "mkfile" "path-base" "not"))
+;; Namespaces
 
-(list . (symbol) @function.macro (#any-of? @function.macro "op" "fn" "current-scope" "quote" "let" "provide" "module" "or" "and" "->" "curryfn" "for" "$" "linux"))
-(cons . (symbol) @function.macro (#any-of? @function.macro "op" "fn" "current-scope" "quote" "let" "provide" "module" "or" "and" "->" "curryfn" "for" "$" "linux"))
+(symbind
+  (symbol) @namespace
+  . (keyword))
 
-(list . (symbol) @keyword.builtin (#any-of? @keyword.builtin "do" "doc"))
-(cons . (symbol) @keyword.builtin (#any-of? @keyword.builtin "do" "doc"))
+;; Includes
 
-(list . (symbol) @include (#any-of? @include "use" "import" "load"))
-(cons . (symbol) @include (#any-of? @include "use" "import" "load"))
+((symbol) @include
+  (#any-of? @include "use" "import" "load"))
 
+;; Keywords
 
-;; special cases
+((symbol) @keyword
+  (#any-of? @keyword "do" "doc"))
 
-; [a & b] highlights & as operator rather than a regular symbol
-(list (symbol) @operator (#match? @operator "&"))
-(cons (symbol) @operator (#match? @operator "&"))
+;; Special Functions
+
+((list
+  . (symbol) @keyword.function
+  . (symbol) @function
+  (symbol)? @parameter)
+  (#any-of? @keyword.function "def" "defop" "defn"))
+
+((symbol) @function.builtin
+  (#any-of? @function.builtin "dump" "mkfs" "json" "log" "error" "now" "cons" "wrap" "unwrap" "eval" "make-scope" "bind" "meta" "with-meta" "null?" "ignore?" "boolean?" "number?" "string?" "symbol?" "scope?" "sink?" "source?" "list?" "pair?" "applicative?" "operative?" "combiner?" "path?" "empty?" "thunk?" "+" "*" "quot" "-" "max" "min" "=" ">" ">=" "<" "<=" "list->source" "across" "emit" "next" "reduce-kv" "assoc" "symbol->string" "string->symbol" "str" "substring" "trim" "scope->list" "string->fs-path" "string->cmd-path" "string->dir" "subpath" "path-name" "path-stem" "with-image" "with-dir" "with-args" "with-cmd" "with-stdin" "with-env" "with-insecure" "with-label" "with-port" "with-tls" "with-mount" "thunk-cmd" "thunk-args" "resolve" "start" "addr" "wait" "read" "cache-dir" "binds?" "recall-memo" "store-memo" "mask" "list" "list*" "first" "rest" "length" "second" "third" "map" "map-pairs" "foldr" "foldl" "concat" "append" "filter" "conj" "list->scope" "merge" "apply" "id" "always" "vals" "keys" "memo" "succeeds?" "run" "last" "take" "collect" "take-all" "insecure!" "from" "cd" "wrap-cmd" "mkfile" "path-base" "not"))
+
+((symbol) @function.macro
+  (#any-of? @function.macro "op" "fn" "current-scope" "quote" "let" "provide" "module" "or" "and" "->" "curryfn" "assert" "for" "$" "linux"))
+
+;; Conditionals
+
+((symbol) @conditional
+  (#any-of? @conditional "if" "case" "cond" "when"))
+
+;; Repeats
+
+((symbol) @repeat
+  (#any-of? @repeat "each"))
+
+;; Operators
+
+; TODO: classify
+((symbol) @operator (#any-of? @operator "&" "*" "+" "-" "<" "<=" "=" ">" ">="))
+
+;; Special forms
+
+; (-> 42 x y) highlights 42 as its default highlight
+(list
+  .
+  (symbol) @function.macro
+  (#eq? @function.macro "->")
+  .
+  (_)
+  (symbol) @function)
 
 ; (-> x y z) highlights first x as var, y z as function
 (list
@@ -91,11 +82,38 @@
   (symbol) @variable.parameter
   (symbol) @function)
 
-; (-> 42 x y) highlights 42 as regular number
-(list
-  .
-  (symbol) @function.macro
-  (#eq? @function.macro "->")
-  .
-  (_)
-  (symbol) @function)
+;; Punctuation
+
+[ "(" ")" ] @punctuation.bracket
+
+[ "{" "}" ] @punctuation.bracket
+
+[ "[" "]" ] @punctuation.bracket
+
+((symbol) @punctuation.delimiter
+  (#eq? @punctuation.delimiter "->"))
+
+;; Literals
+
+(string) @string
+
+(escape_sequence) @string.escape
+
+(path) @text.uri @string.special
+
+(number) @number
+
+(boolean) @boolean
+
+[
+  (ignore)
+  (null)
+] @constant.builtin
+
+[
+  "^"
+] @character.special
+
+;; Comments
+
+(comment) @comment @spell
